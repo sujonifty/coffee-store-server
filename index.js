59ff0eacd5c7f19ddaa2cff1104c9c01388e45b7
sujonifty <sujonifty@gmail.com>
2024-04-22 11:39:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//connect to database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b9hcdyj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const coffeeCollection = client.db('coffeeDB').collection('coffee');
+    //Receive data from the client server
+    app.post('/coffee',async(req, res)=>{
+        const newCoffee = req.body;
+        console.log(newCoffee);
+        // insert data in database
+        const result = await coffeeCollection.insertOne(newCoffee);
+        res.send(result);
+    })
+    // get data from database
+    app.get('/coffee', async(req, res)=>{
+        const cursor = coffeeCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/',(req, res)=>{
+    res.send('coffee making server');
+
+})
+app.listen(port,()=>{
+    console.log(`coffee making server ${port}`);
+
+})
+
+
