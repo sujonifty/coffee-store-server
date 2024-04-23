c852b559c2f918021e6400aeaa3565132f239397
sujonifty <sujonifty@gmail.com>
2024-04-23 08:49:06 +0600

    delete and updated section is completed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000
 
@@ -39,6 +39,40 @@ async function run() {
     app.get('/coffee', async(req, res)=>{
         const cursor = coffeeCollection.find();
         const result = await cursor.toArray();
+        res.send(result);//we can access data in client server(like app-route) by using loader(fetch)
+    })
+
+    app.get('/coffee/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await coffeeCollection.findOne(query);
+        res.send(result);
+    })
+    // get data from database
+    app.put('/coffee/:id', async(req, res)=>{
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)};
+        const options ={ upsert:true};
+        const updatedCoffee =req.body;
+        const coffee={
+            $set:{
+                name:updatedCoffee.name,
+                chef:updatedCoffee.chef,
+                supplier:updatedCoffee.supplier,
+                taste:updatedCoffee.taste,
+                category:updatedCoffee.category,
+                details:updatedCoffee.details,
+                photo:updatedCoffee.photo,
+            }
+        }
+        const result = await coffeeCollection.updateOne(filter,coffee,options);
+        res.send(result);
+    })
+    // deleted data from database
+    app.delete('/coffee/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await coffeeCollection.deleteOne(query);
         res.send(result);
     })
     // Send a ping to confirm a successful connection
